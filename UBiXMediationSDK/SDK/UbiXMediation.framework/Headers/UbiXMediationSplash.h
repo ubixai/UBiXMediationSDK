7dc62faf6a7cffa1c4f7813297d2cd4dce456eee
guoqiang <guoqiang.zhu@ubixai.com>
2025-05-30 12:38:24 +0800

    update 2.8.2.0002

--- UBiXMediationSDK/SDK/UbiXMediation.framework/Headers/UbiXMediationSplash.h
+++ UBiXMediationSDK/SDK/UbiXMediation.framework/Headers/UbiXMediationSplash.h
@@ -57,7 +57,7 @@ typedef NS_ENUM(NSUInteger, UbiXMSkipButtonShowType) {
 /**
  * 在Deleggate  中获取生成的 token
  */
-- (void)buildBiddingToken;
+- (NSString *)buildBiddingToken;
 
 - (void)loadAdWithAdm:(NSString *)adm;
 
